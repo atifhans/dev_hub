8d0b0b5e27348346c9b847fac0855968333ce834
Atif Iqbal <atif.hans@gmail.com>
2015-12-25 03:30:46 -0800

    Finish add_users

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,4 @@
 Rails.application.routes.draw do
-  #get 'static_pages/home'
-
-  #get 'static_pages/help'
-
-  #get 'static_pages/about'
-
-  #get 'static_pages/contact'
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -15,6 +7,8 @@ Rails.application.routes.draw do
   get  'help'     => 'static_pages#help'
   get  'about'    => 'static_pages#about'
   get  'contact'  => 'static_pages#contact'
+  get  'signup'   => 'users#new'
+  resources :users
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
